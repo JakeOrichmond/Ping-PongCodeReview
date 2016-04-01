6a69f0226776806906a1cbdd4b86fb5c8858d48a
jr <jakeorichmond@gmail.com>
2016-04-01 12:32:15 -0700

    add comment and make pretty

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 // BUSINESS LOGIC //
-for (var index = 1; index <= numberInput; index += 1) {
-  if index % 3 ===0 && index % 5 === 0) {
-    ()
+function pingPong(userEntry) {
+  var result = "";
+  for (var index = 1; index <= userEntry; index += 1) {
+    if (index % 3 ===0 && index % 5 === 0) {
+      $("#result").append("<li>" + "ping-pong" + "</li>");
+    }
+    else if ( index  % 3 === 0) {
+      $("#result").append("<li>" + "ping" + "</li>");
+    }
+    else if ( index % 5 === 0) {
+      $("#result").append( "<li>" + "pong" + "</li>")
+    }
+    else {
+      $("#result").append("<li>" + index + "</li>");
+    }
   }
 }
 
-
 // UI LOGIC //
 $(document).ready(function() {
-console.log("fire away little buddy.")
-$("form#pingOrPong").submit(function(event) {
-  var firstNumber = $("input#firstNumber").val();
-  var result =
-})
-
+  console.log("fire away little buddy.")
+  $("#game").submit(function(event) {
+    event.preventDefault();
+    var entry = parseInt($("input").val());
+    pingPong(entry);
+    $("#result").show();
+  });
 });
