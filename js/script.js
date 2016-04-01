046832055d7f9937fcd0470fa6ec18c2047e1c36
jr <jakeorichmond@gmail.com>
2016-04-01 11:36:00 -0700

    Add business Logic

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// BUSINESS LOGIC //
+for (var index = 1; index <= numberInput; index += 1) {
+  if index % 3 ===0 && index % 5 === 0) {
+    ()
+  }
+}
+
+
+// UI LOGIC //
+$(document).ready(function() {
+console.log("fire away little buddy.")
+$("form#pingOrPong").submit(function(event) {
+  var firstNumber = $("input#firstNumber").val();
+  var result =
+})
+
+});
